1d6e0100982bbbebf29f414f932f663b52d021e5
Hamza Tahir <hamza@zenml.io>
2024-10-27 15:14:38 +0100

    Add pagination support to list methods

--- internal/provider/client.go
+++ internal/provider/client.go
@@ -57,7 +57,13 @@ func (c *Client) doRequest(method, path string, body interface{}) (*http.Respons
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		defer resp.Body.Close()
-		return nil, fmt.Errorf("API request failed with status %d", resp.StatusCode)
+		var apiError APIError
+		if err := json.NewDecoder(resp.Body).Decode(&apiError); err != nil {
+			// If we can't decode the error response, return a generic error
+			body, _ := io.ReadAll(resp.Body)
+			return nil, fmt.Errorf("API request failed with status %d: %s", resp.StatusCode, string(body))
+		}
+		return nil, &apiError
 	}
 
 	return resp, nil
@@ -247,6 +253,53 @@ func (c *Client) ListStacks(params *ListParams) (*Page[StackResponse], error) {
 	}
 
 	var result Page[StackResponse]
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("error decoding response: %v", err)
+	}
+
+	return &result, nil
+}
+
+// Add pagination support to all list methods
+func (c *Client) ListStackComponents(params *ListParams) (*Page[ComponentResponse], error) {
+	url := "/api/v1/components"
+	if params != nil {
+		url = fmt.Sprintf("%s?page=%d&size=%d", url, params.Page, params.PageSize)
+		for k, v := range params.Filter {
+			url = fmt.Sprintf("%s&%s=%s", url, k, v)
+		}
+	}
+
+	resp, err := c.doRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result Page[ComponentResponse]
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("error decoding response: %v", err)
+	}
+
+	return &result, nil
+}
+
+func (c *Client) ListServiceConnectors(params *ListParams) (*Page[ServiceConnectorResponse], error) {
+	url := "/api/v1/service_connectors"
+	if params != nil {
+		url = fmt.Sprintf("%s?page=%d&size=%d", url, params.Page, params.PageSize)
+		for k, v := range params.Filter {
+			url = fmt.Sprintf("%s&%s=%s", url, k, v)
+		}
+	}
+
+	resp, err := c.doRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result Page[ServiceConnectorResponse]
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return nil, fmt.Errorf("error decoding response: %v", err)
 	}
