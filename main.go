e0e1b64a3a94316c533b0270a4d860713dca75df
Hamza Tahir <hamza@zenml.io>
2024-10-27 14:12:39 +0100

    Update schema default values for server_url and api_key

--- main.go
+++ main.go
@@ -11,26 +11,24 @@ func main() {
 	})
 }
 
-// Provider returns a terraform.ResourceProvider.
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"server_url": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Sensitive:   false,
-				Description: "ZenML server URL",
+				DefaultFunc: schema.EnvDefaultFunc("ZENML_SERVER_URL", nil),
 			},
 			"api_key": {
 				Type:        schema.TypeString,
 				Required:    true,
 				Sensitive:   true,
-				Description: "ZenML API key",
+				DefaultFunc: schema.EnvDefaultFunc("ZENML_API_KEY", nil),
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"zenml_stack":            resourceStack(),
-			"zenml_stack_component": resourceStackComponent(),
+			"zenml_stack":             resourceStack(),
+			"zenml_stack_component":   resourceStackComponent(),
 			"zenml_service_connector": resourceServiceConnector(),
 		},
 	}
